6e930727ba771b1c338753ece2bd2ad0aec361fe
Đình Nam <namndwebdev@gmail.com>
2024-04-24 15:39:36 +0700

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,60 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Child from './Child'
-
+import axios from 'axios'
+import Login from './components/Login'
+import './App.css'
 export default function App(){
-    const [data, setData] = useState([])
+    const [tasks, setTasks] = useState([])
+    
+    useEffect(()=>{
+        let config = {
+            method: 'get',
+            url: 'https://backoffice.nodemy.vn/api/tasks?populate=*&pagination[pageSize]=10&pagination[page]=1&sort[0]=createdAt:desc',
+            headers: { }
+        };
+        
+        axios.request(config)
+        .then((response) => {
+            let data = response.data
+            let arr = data.data
+            
+            console.log(arr);
+            setTasks(arr)
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, [])
+
+
     const inputRef = useRef(null)
     function add(){
-        data.push(inputRef.current.value)
-        setData([...data])
+        let data = JSON.stringify({
+            "data": {
+              "title": inputRef.current.value
+            }
+          });
+          
+          let config = {
+            method: 'post',
+            url: 'https://backoffice.nodemy.vn/api/tasks',
+            headers: { 
+              'Content-Type': 'application/json', 
+              'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            data : data
+          };
+          
+          axios.request(config)
+          .then((response) => {
+            console.log('ban da them thanh cong');
+            let objectMoiThem = response.data.data
+            tasks.unshift(objectMoiThem)
+            setTasks([...tasks])
+          })
+          .catch((error) => {
+            console.log(error);
+          });
     }
     
     function xoa(giaTriMuonXoa){
@@ -25,16 +73,23 @@ export default function App(){
     }
 
     return (<>
-        <input type="text" ref={inputRef}/>
-        <button onClick={add}>Them</button>
+        <div className="container">
+            <div className='sidebar'>
+                <Login/>
+            </div>
+            <div className='content'>
+                <input type="text" ref={inputRef}/>
+                <button onClick={add}>Them</button>
 
-        {data.map(item=>{
-            return <Child 
-                key={Math.random()}
-                name={item}
-                xoa={xoa}
-                sua={sua}
-                />
-        })}
+                {tasks.map(item=>{
+                    return <Child 
+                        key={item?.id}
+                        name={item?.attributes?.title}
+                        xoa={xoa}
+                        sua={sua}
+                        />
+                })}
+            </div>
+        </div>
     </>)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import Login from './components/Login'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App/> */}
-    <Login/>
+    <App/>
+    {/* <Login/> */}
   </React.StrictMode>,
 )
