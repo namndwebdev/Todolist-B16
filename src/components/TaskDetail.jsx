4f58f65aa0c4cbd42fe7fd8706dadd3df1bd390e
Đình Nam <namndwebdev@gmail.com>
2024-05-04 15:55:23 +0700

    hoan thanh sua xoa o trang chi tiet

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import axios from 'axios'
 export default function TaskDetail(){
     const params = useParams()
     const [task, setTask] = useState({})
-
+    const nav = useNavigate()
     useEffect(()=>{
         let config = {
             method: 'get',
@@ -20,10 +20,60 @@ export default function TaskDetail(){
           });
     }, [])
 
+    const handleChange = (e)=>{
+      const key = e.target.getAttribute('name')
+      task.attributes[key] = e.target.value
+      setTask({...task})
+    }
+
+    const update = ()=>{
+      let data = JSON.stringify({
+        "data": task.attributes
+      });
+      
+      let config = {
+        method: 'put',
+        maxBodyLength: Infinity,
+        url: `https://backoffice.nodemy.vn/api/tasks/${params.id}`,
+        headers: { 
+          'Content-Type': 'application/json', 
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        data : data
+      };
+      
+      axios.request(config)
+      .then((response) => {
+        alert('Update Thanh cong')
+      })
+      .catch((error) => {
+        alert('Update That bai')
+      });
+    }
+
+    function deleteTask(){
+      let config = {
+        method: 'delete',
+        url: `https://backoffice.nodemy.vn/api/tasks/${params.id}`,
+        headers: { 
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      };
+
+      axios.request(config)
+      .then((response) => {
+        nav('/')
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
     return (<>
         <h1>Chi tiet Task {task?.id}</h1>
-        <h2>Task : {task?.attributes?.title}</h2>
-        <h2>Hoan thanh: {task?.attributes?.complete + ""}</h2>
-        <h2>Deadline: {task?.attributes?.date}</h2>
+        <h2>Task : {task?.attributes?.title}</h2> <input name="title" type="text" value={task?.attributes?.title} onChange={handleChange}/>
+        <h2>Hoan thanh: {task?.attributes?.complete + ""}</h2> <input name="complete" type="text" value={task?.attributes?.complete} onChange={handleChange}/>
+        <h2>Deadline: {task?.attributes?.date}</h2> <input name="date" type="text" value={task?.attributes?.date} onChange={handleChange}/>
+        <button onClick={update}>Sửa</button>
+        <button onClick={deleteTask}>Xoá</button>
     </>)
 }
